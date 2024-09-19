cd5a1307513caaa71eaa8366a04f4a5bfdf66d5f
Nelson Belgarde <nelsonbelgarde@gmail.com>
2024-09-19 18:42:50 +0200

    core: Add Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sandbox.app',
+  appName: 'sandbox',
+  webDir: 'dist/sandbox'
+};
+
+export default config;

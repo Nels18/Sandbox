d40a77fe44d51495edfe6a7033e2a7f2c571f0ca
Nelson Belgarde <nelsonbelgarde@gmail.com>
2024-09-20 08:57:24 +0200

    chore: Install the native platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sandbox.app',
   appName: 'sandbox',
-  webDir: 'dist/sandbox'
+  webDir: 'dist/sandbox/browser',
 };
 
 export default config;
